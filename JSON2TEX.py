3bc914db524385fcefacaf1e7f6dcbb50326dfa2
Reto Fluetsch <rfluetsch@ethz.ch>
2023-07-19 22:37:27 +0200

    added rudimentary stat block rendering

--- JSON2TEX.py
+++ JSON2TEX.py
@@ -4,7 +4,167 @@ import warnings
 import re
 
 class TexRenderer:
-    lines = []
+
+    class StatBlockRenderer:
+        def __init__(self):
+            self.lines = []
+
+        def renderInlineStatBlock(self, data):
+            monsterData = data.get("data")
+            self.lines.append("\\begin{DndMonster}[width=\\textwidth / 2 + 8pt]{@MONSTERNAME}\n\\begin{multicols}{2}".replace("@MONSTERNAME", monsterData.get("name")))
+            self.renderMonsterType(monsterData)
+            self.renderAbilityScores(monsterData)
+            self.renderMonsterBasics(monsterData)
+            self.renderMonsterDetails(monsterData)
+            self.renderMonsterActions(monsterData)
+            self.lines.append("\\end{multicols}\n\\end{DndMonster}")
+            return self.lines
+        
+        def renderMonsterType(self, monsterData):
+            sizes = {"T": "Tiny", "S": "Small", "M": "Medium", "L": "Large", "H": "Huge", "G": "Gargantuan"}
+            alignmentS = {"L": "Lawful", "C": "Chaotic", "N": "Neutral", "G": "Good", "E": "Evil", "T": "True neutral", "U": "Unaligned"}
+            typeStrs = []
+            for size in monsterData.get("size"):
+                typeStrs.append(sizes.get(size))
+            typeStrs.append(monsterData.get("type").capitalize() + ",")
+            for alignment in monsterData.get("alignment"):
+                typeStrs.append(alignmentS.get(alignment))
+            self.lines.append("\\DndMonsterType{@MONSTERTYPE}".replace("@MONSTERTYPE", " ".join(typeStrs)))
+
+        def renderAbilityScores(self, monsterData):
+            scores = "\\DndMonsterAbilityScores[str = @STR,dex = @DEX,con = @CON,int = @INT,wis = @WIS,cha = @CHA]"
+            scores = scores.replace("@STR", str(monsterData.get("str")))
+            scores = scores.replace("@DEX", str(monsterData.get("dex")))
+            scores = scores.replace("@CON", str(monsterData.get("con")))
+            scores = scores.replace("@INT", str(monsterData.get("int")))
+            scores = scores.replace("@WIS", str(monsterData.get("wis")))
+            scores = scores.replace("@CHA", str(monsterData.get("cha")))
+            self.lines.append(scores)
+
+        def renderMonsterBasics(self, monsterData):
+            basicStr = "\\DndMonsterBasics[armor-class = {@AC}, hit-points  = {@HP}, speed = {@SPEED}]"
+            hp = "{@dice @FORMULA}".replace("@FORMULA", monsterData.get("hp").get("formula"))
+            basicStr = basicStr.replace("@HP", hp)
+            speed = self.renderSpeed(monsterData.get("speed"))
+            basicStr = basicStr.replace("@SPEED", speed)
+            ac = self.renderAC(monsterData.get("ac"))
+            basicStr = basicStr.replace("@AC", ac)
+            self.lines.append(basicStr)
+
+        def renderSpeed(self, speedData):
+            speeds = []
+            for type in speedData.keys():
+                if isinstance(speedData.get(type), int): 
+                    speeds.append("@TYPE @SPEED ft.".replace("@TYPE", type).replace("@SPEED", str(speedData.get(type))))
+                else:
+                    speed = speedData.get(type)
+                    speeds.append("@TYPE @SPEED ft. @COND".replace("@TYPE", type).replace("@SPEED", str(speed.get("number"))).replace("@COND", speed.get("condition")))
+            return ", ".join(speeds)
+
+        def renderMonsterSpellcasting(self, monsterData):
+            self.lines.append("\\DndMonsterSection{Spellcasting}")
+            self.lines.append("Spellcasting is NOT CURRENTLY IMPLEMENTED\n")
+
+        def renderAC(self, acData):
+            ACs = []
+            for ac in acData:
+                if isinstance(ac, int): 
+                    ACs.append(str(ac))
+                else:
+                    AC = str(ac.get("ac"))
+                    if "from" in ac:
+                        AC += " ( " + ", ".join(ac.get("from")) + ")"
+                    if "condition" in ac:
+                        AC += " @COND".replace("@COND", ac.get("condition"))
+                    ACs.append(AC)
+            return ", ".join(ACs)
+
+        def renderMonsterDetails(self, monsterData):
+            detailsStr = "\\DndMonsterDetails["
+            details = []
+            if "save" in monsterData:
+                details.append("saving-throws = {" + self.renderListedDetails(monsterData.get("save")) + "}")
+            if "skill" in monsterData:
+                details.append("skills = {" + self.renderListedDetails(monsterData.get("skill")) + "}")
+            if "vulnerable" in monsterData:
+                details.append("damage-vulnerabilities = {" + self.renderGroupedDetails(monsterData.get("vulnerable"), "vulnerable") + "}")
+            if "resist" in monsterData:
+                details.append("damage-resistances = {" + self.renderGroupedDetails(monsterData.get("resist"), "resist") + "}")
+            if "immune" in monsterData:
+                details.append("damage-immunities = {" + self.renderGroupedDetails(monsterData.get("immune"), "immune") + "}")
+            if "conditionImmune" in monsterData:
+                details.append("condition-immunities = {" + self.renderGroupedDetails(monsterData.get("conditionImmune"), "conditionImmune") + "}")
+            if "senses" in monsterData:
+                senses = monsterData.get("senses")
+                senses.append("Passive Perception " + str(monsterData.get("passive")))
+                sensesStr = ", ".join(senses).replace(", " + senses[-1], " and " + senses[-1])
+                details.append("senses = {" + sensesStr + "}")
+            if "languages" in monsterData:
+                languages = monsterData.get("languages")
+                languagesStr = ", ".join(languages).replace(", " + languages[-1], " and " + languages[-1])
+                details.append("languages = {" + languagesStr + "}")
+            if "cr" in monsterData:
+                details.append("challenge = {" + monsterData.get("cr") + "}")
+            detailsStr += ", ".join(details) + "]"
+            self.lines.append(detailsStr)
+
+        def renderGroupedDetails(self, detailData, detailName):
+            vunerableStrings = []
+            for vun in detailData:
+                if isinstance(vun, str): vunerableStrings.append(vun)
+                else:
+                    pre = ""
+                    post = ""
+                    if "preNote" in vun: pre = vun.get("preNote")
+                    if "note" in vun: post = vun.get("note")
+                    vunerableStrings.append( pre + " " + self.renderGroupedDetails(vun.get(detailName), detailName) + " " + post )
+            returnStr = ", ".join(vunerableStrings)
+            return returnStr.replace(", " + vunerableStrings[-1], " and " + vunerableStrings[-1])
+                    
+        def renderListedDetails(self, detailData):
+            skillStrs = []
+            for skill in detailData:
+                skillStrs.append("{@skill " + skill.capitalize() + "} " + detailData.get(skill))
+            returnStr = ", ".join(skillStrs)
+            return returnStr.replace(", " + skillStrs[-1], " and " + skillStrs[-1])
+
+        def renderMonsterActions(self, monsterData):
+            if "trait" in monsterData:
+                self.renderActions(monsterData.get("trait"))
+            if "spellcasting" in monsterData:
+                self.renderMonsterSpellcasting(monsterData)
+            if "action" in monsterData:
+                self.lines.append("\\DndMonsterSection{@TITLE}".replace("@TITLE", "Actions"))
+                if "actionHeader" in monsterData:
+                    for entry in monsterData.get("actionHeader"): self.lines.append(entry)
+                self.renderActions(monsterData.get("action"))
+            if "bonus" in monsterData:
+                self.lines.append("\\DndMonsterSection{@TITLE}".replace("@TITLE", "Bonusactions"))
+                if "bonusHeader" in monsterData:
+                    for entry in monsterData.get("bonusHeader"): self.lines.append(entry)
+                self.renderActions(monsterData.get("bonus"))
+            if "reaction" in monsterData:
+                self.lines.append("\\DndMonsterSection{@TITLE}".replace("@TITLE", "Reactions"))
+                if "reactionHeader" in monsterData:
+                    for entry in monsterData.get("reactionHeader"): self.lines.append(entry)
+                self.renderActions(monsterData.get("reaction"))
+            if "legendary" in monsterData:
+                self.lines.append("\\DndMonsterSection{@TITLE}".replace("@TITLE", "Legendary Actions"))
+                if "legendaryHeader" in monsterData:
+                    for entry in monsterData.get("legendaryHeader"): self.lines.append(entry)
+                self.renderActions(monsterData.get("legendary"))
+            if "mythic" in monsterData:
+                self.lines.append("\\DndMonsterSection{@TITLE}".replace("@TITLE", "Mythic Actions"))
+                if "mythicHeader" in monsterData:
+                    for entry in monsterData.get("mythicHeader"): self.lines.append(entry)
+                self.renderActions(monsterData.get("mythic"))
+
+        def renderActions(self, actions):
+            for trait in actions:
+                self.lines.append("\\DndMonsterAction{@TRAITNAME}".replace("@TRAITNAME", trait.get("name")))
+                for entry in trait.get("entries"):
+                    self.lines.append(entry + "\n")
+
     f_name = "UnnamedFile"
     temp_name = "5eJSON2TEX_UnnamedFile"
     titles = [
@@ -121,6 +281,8 @@ class TexRenderer:
             self.renderQuote(data)
         elif data.get("type") == "image":
             self.renderImage(data)
+        elif data.get("type") == "statblockInline":
+            self.renderStatblock(data)
         else:
             warnings.warn("\nThe following type of JSON-Entry has not yet been implemented: " + data.get("type") + "\nThe entry will be skipped!", category=RuntimeWarning)
 
@@ -197,6 +359,12 @@ class TexRenderer:
         self.appendLine("\\end{figure}\n\\endgroup")'''
         warnings.warn("\nImage rendering currently disabled", category=RuntimeWarning)
 
+    def renderStatblock(self, data):
+        renderer = TexRenderer.StatBlockRenderer()
+        linesToAdd = renderer.renderInlineStatBlock(data)
+        for line in linesToAdd:
+            self.appendLine(line)
+
     '''
     Adds a simple string to the 'lines' container
     '''
@@ -237,7 +405,7 @@ class TexRenderer:
         elif re.findall("{@code .*?}", tag):
             return line.replace(tag, tag.replace("{@code ", "\\texttt{"))
         elif re.findall("{@dice .*?}", tag):
-            return line.replace(tag, tag.replace("{@dice ", "").replace("}", ""))
+            return line.replace(tag, tag.replace("{@dice ", "\\DndDice{"))
         elif re.findall("{@skill .*?}", tag):
             return line.replace(tag, tag.replace("{@skill ", "\\textit{"))
         else:
