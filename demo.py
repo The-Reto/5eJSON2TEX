09c5b13fb19f437025586e23aecf7aceaeb93037
The-Reto <fluetsch@the-reto.ch>
2023-07-24 12:27:31 +0200

    Added commets to demo.py

--- demo.py
+++ demo.py
@@ -1,5 +1,7 @@
 from JSON2TEX import TexRenderer
 
 renderer = TexRenderer()
-renderer.renderAdventure("./AnExampleAdventure.json")
-renderer.convertToPdf()
+
+renderer.renderAdventure("./AnExampleAdventure.json") #Renders the adventure and writes it to a .tex file
+
+renderer.convertToPdf() #converts the .tex file to a .pdf file and cleans up the working directory
